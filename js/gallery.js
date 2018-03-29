75a205282c5026318e9f03f3768246b743fb51bb
shimshamz <31662802+shimshamz@users.noreply.github.com>
2018-03-28 21:34:07 -0400

    Update GalleryImage function

--- js/gallery.js
+++ js/gallery.js
@@ -79,10 +79,14 @@ window.addEventListener('load', function() {
 
 }, false);
 
-function GalleryImage() {
+function GalleryImage(location, description, date, img) {
 	//implement me as an object to hold the following data about an image:
 	//1. location where photo was taken
+	this.location = location;
 	//2. description of photo
+	this.description = description;
 	//3. the date when the photo was taken
+	this.date = date;
 	//4. either a String (src URL) or an an HTMLImageObject (bitmap of the photo. https://developer.mozilla.org/en-US/docs/Web/API/HTMLImageElement)
+	this.img = img;
 }
